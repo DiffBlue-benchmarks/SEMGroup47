5bdec8387eb05cd132797224ec413f0254cc66bb
Bas de Bock <bdebock@student.tudelft.nl>
2015-09-24 09:34:31 +0200

    fix <bug

--- src/main/java/sem/group47/entity/Player.java
+++ src/main/java/sem/group47/entity/Player.java
@@ -85,19 +85,18 @@ public class Player extends MapObject {
 			e.printStackTrace();
 		}
 
-		// AudioPlayer.load("/sfx/jump.wav", "jump");
-		// AudioPlayer.load("/sfx/fire_bubble.wav", "fire");
-		// AudioPlayer.load("/sfx/extra_life.wav", "extraLife");
-		// AudioPlayer.load("/sfx/bubble_pop.wav", "bubblePop");
-		// AudioPlayer.load("/sfx/player_death.wav", "dead");
-		// AudioPlayer.load("/sfx/crash.wav", "crash");
+		try {
+			AudioPlayer.load("/sfx/jump.wav", "jump");
+			AudioPlayer.load("/sfx/fire_bubble.wav", "fire");
+			AudioPlayer.load("/sfx/extra_life.wav", "extraLife");
+			AudioPlayer.load("/sfx/bubble_pop.wav", "bubblePop");
+			AudioPlayer.load("/sfx/player_death.wav", "dead");
+			AudioPlayer.load("/sfx/crash.wav", "crash");
+		} catch (Exception e) {
+			Log.error("IO Read", "Could not file music");
+			e.printStackTrace();
+		}
 
-		AudioPlayer.load("/music/menu.mp3", "jump");
-		AudioPlayer.load("/music/menu.mp3", "fire");
-		AudioPlayer.load("/music/menu.mp3", "extraLife");
-		AudioPlayer.load("/music/menu.mp3", "bubblePop");
-		AudioPlayer.load("/music/menu.mp3", "dead");
-		AudioPlayer.load("/music/menu.mp3", "crash");
 	}
 
 	/**
