2cfde2caf7e6f16930639631bab537e38ffdd336
Lilian Lehmann <l.c.lehmann@student.tudelft.nl>
2015-09-25 22:46:18 +0200

    Protected to Public
    Hope to fix the error

--- src/main/java/sem/group47/entity/pickups/PickupObject.java
+++ src/main/java/sem/group47/entity/pickups/PickupObject.java
@@ -11,61 +11,67 @@ import sem.group47.tilemap.TileMap;
 
 /**
  * Object which can be pickedup by the player and has a certain effect
+ * 
  * @author Christian
- *
+ * 
  */
 public abstract class PickupObject extends MapObject {
-
- /**
-  * constructor
-  * @param tm TileMap
-  */
- public PickupObject(final TileMap tm) {
-  super(tm);
-  setWidth(32);
-  setHeight(32);
-  setCwidth(32);
-  setCheight(32);
-  setFallSpeed(.35);
-  setMaxFallSpeed(6.0);
-  
-  try {
-   BufferedImage spritesheet = ImageIO.read(getClass()
-     .getResourceAsStream("/items/items.png"));
-   setSprite(spritesheet.getSubimage(
-     (int) Math.round(Math.random() * 7) * 32,
-     (int) Math.round(Math.random() * 7) * 32,
-     32,
-     32));
-  } catch (Exception e) {
-   Log.error("IO Read", "Could not file player sprite");
-   e.printStackTrace();
-  }
- }
-
- /**
-  * Checks whether the object collides with the player, if so executes onPickup(p), activating the powerup.
-  * @param p Player object
-  * @return true if collision occurred
-  */
- public final boolean checkCollision(final Player p) {
-  if (p.intersects(this)) {
-   onPickup(p);
-   return true;
-  }
-  return false;
- }
-
- /**
-  * Effect of getting picked up.
-  * @param p Player object
-  */
- protected abstract void onPickup(final Player p);
-
- @Override
- public final void update() {
-  setDy(getDy() + getFallSpeed());
-  checkTileMapCollision();
-  setPosition(getXposNew(), getYposNew());
- }
+	
+	/**
+	 * constructor
+	 * 
+	 * @param tm
+	 *           TileMap
+	 */
+	public PickupObject(final TileMap tm) {
+		super(tm);
+		setWidth(32);
+		setHeight(32);
+		setCwidth(32);
+		setCheight(32);
+		setFallSpeed(.35);
+		setMaxFallSpeed(6.0);
+		
+		try {
+			BufferedImage spritesheet = ImageIO.read(getClass()
+					.getResourceAsStream("/items/items.png"));
+			setSprite(spritesheet.getSubimage(
+					(int) Math.round(Math.random() * 7) * 32,
+					(int) Math.round(Math.random() * 7) * 32, 32, 32));
+		} catch (Exception e) {
+			Log.error("IO Read", "Could not file player sprite");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Checks whether the object collides with the player, if so executes
+	 * onPickup(p), activating the powerup.
+	 * 
+	 * @param p
+	 *           Player object
+	 * @return true if collision occurred
+	 */
+	public final boolean checkCollision(final Player p) {
+		if (p.intersects(this)) {
+			onPickup(p);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Effect of getting picked up.
+	 * 
+	 * @param p
+	 *           Player object
+	 */
+	public abstract void onPickup(final Player p);
+	
+	@Override
+	public final void update() {
+		setDy(getDy() + getFallSpeed());
+		checkTileMapCollision();
+		setPosition(getXposNew(), getYposNew());
+	}
 }
