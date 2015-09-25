6fff0620ced6ebfcf7e2764c902204d3a28a116e
Lilian Lehmann <l.c.lehmann@student.tudelft.nl>
2015-09-25 22:16:42 +0200

    Tests multiplayer & powerups

--- src/test/java/sem/group47/gamestate/GameStateManagerTest.java
+++ src/test/java/sem/group47/gamestate/GameStateManagerTest.java
@@ -9,19 +9,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sem.group47.main.Log;
+import sem.group47.tilemap.TileMap;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class GameStateManagerTest.
  */
 public class GameStateManagerTest {
-
+	
 	/** The g2d. */
 	private Graphics2D g2d;
-
+	
 	/** The gsm. */
 	public GameStateManager gsm;
-
+	
+	public TileMap tileMap;
+	
 	/**
 	 * Sets the up.
 	 */
@@ -32,7 +35,7 @@ public class GameStateManagerTest {
 				BufferedImage.TYPE_INT_RGB);
 		g2d = (Graphics2D) image.getGraphics();
 	}
-
+	
 	/**
 	 * Test game update.
 	 */
@@ -42,11 +45,11 @@ public class GameStateManagerTest {
 		assertEquals(gsm.getCurrentState(), GameStateManager.MENUSTATE);
 		gsm.setState(GameStateManager.LEVELSTATE);
 		assertEquals(gsm.getCurrentState(), GameStateManager.LEVELSTATE);
-		for(int i = 0; i < 6000; i++) {
+		for (int i = 0; i < 6000; i++) {
 			gsm.update();
 		}
 	}
-
+	
 	/**
 	 * Test game update and draw.
 	 */
@@ -56,10 +59,26 @@ public class GameStateManagerTest {
 		assertEquals(gsm.getCurrentState(), GameStateManager.MENUSTATE);
 		gsm.setState(GameStateManager.LEVELSTATE);
 		assertEquals(gsm.getCurrentState(), GameStateManager.LEVELSTATE);
-		for(int i = 0; i < 6000; i++) {
+		for (int i = 0; i < 6000; i++) {
 			gsm.update();
 			gsm.draw(g2d);
 		}
 	}
-
+	
+	// @Test
+	// public final void testMultiPlayer() {
+	// PlayerSave.setMultiplayerEnabled(true);
+	// LevelState ls = new LevelState(gsm);
+	// ls.player1 = new Player(tileMap);
+	// ls.player2 = new Player(tileMap);
+	// assertEquals(ls.player1.getLives(), ls.player2.getLives());
+	// assertEquals(ls.player1.getScore(), ls.player2.getScore());
+	// ls.player1.hit(1);
+	// assertEquals(ls.player1.getLives(), 2);
+	// assertEquals(ls.player2.getLives(), 3);
+	// ls.player2.hit(4);
+	// assertEquals(ls.player2.getIsAlive(), false);
+	// assertEquals(ls.player1.getIsAlive(), true);
+	// }
+	
 }
