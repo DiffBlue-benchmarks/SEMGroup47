f099fa5c69486b3461e180e31f135410c4f3e8db
Christian <cmaulany@tudelft.nl>
2015-10-13 16:05:04 +0200

    Magiron
    Aaron appears after 45 seconds and 'haunts' the player
    <feature

--- src/main/java/sem/group47/entity/enemies/Magiron.java
+++ src/main/java/sem/group47/entity/enemies/Magiron.java
@@ -1,26 +1,23 @@
 package sem.group47.entity.enemies;
 
-import java.awt.Graphics2D;
-
 import javax.imageio.ImageIO;
 
-import sem.group47.gamestate.LevelState;
+import sem.group47.entity.MapObject;
+import sem.group47.entity.Player;
 import sem.group47.tilemap.TileMap;
 
-public class Magiron extends Enemy {
-
-	public int i;
+public class Magiron extends Enemy {	
+	private MapObject target;
 
 	public Magiron(TileMap tm) {
 		super(tm);
 
 		setScorePoints(100);
-		setWidth(30);
-		setHeight(30);
+		setWidth(90);
+		setHeight(112);
 		setCwidth(30);
 		setCheight(30);
-		setMovSpeed(1);
-		setMaxSpeed(2);
+		setMovSpeed(1.3);
 
 		setFacingRight(true);
 
@@ -32,130 +29,44 @@ public class Magiron extends Enemy {
 
 		try {
 			setSprite(ImageIO.read(getClass().getResourceAsStream(
-					"/hud/Bubble_Heart.png")));
+					"/enemies/magiaaron.gif")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	private void getNextXPosition() {
-		if (isRight()) {
-			setDx(getDx() + getMovSpeed());
-			if (getDx() > getMaxSpeed()) {
-				setDx(getMaxSpeed());
-			}
-		} else if (isLeft()) {
-			setDx(getDx() - getMovSpeed());
-			if (getDx() < -getMaxSpeed()) {
-				setDx(-getMaxSpeed());
-			}
-		}
-		if (getDx() > 0) {
-			setFacingRight(true);
-		} else if (getDx() < 0) {
-			setFacingRight(false);
-		}
-	}
-
-	private void getNextYPosition() {
-		if (isUp()) {
-			setDy(getDy() - getMovSpeed());
-			if (getDy() < -getMaxSpeed()) {
-				setDy(-getMaxSpeed());
-			}
-		} else if (isDown()) {
-			setDy(getDy() + getMovSpeed());
-			if (getDy() > getMaxSpeed()) {
-				setDy(getMaxSpeed());
-			}
-		}
-	}
-
-	private void movementDice() {
-		double dice = Math.random() * 100;
-		if (dice < 25) {
-			setUp(true);
-			setDown(false);
-			setRight(false);
-			setLeft(false);
-		} else if (dice < 50) {
-			setLeft(true);
-			setRight(false);
-			setUp(false);
-			setDown(false);
-		} else if (dice < 75) {
-			setLeft(false);
-			setRight(true);
-			setUp(false);
-			setDown(false);
-		} else {
-			setDown(true);
-			setUp(false);
-			setLeft(false);
-			setRight(false);
-		}
+	
+	public void setTarget(MapObject t) {
+		target = t;
 	}
 
 	@Override
 	public void update() {
-		if (System.currentTimeMillis() - LevelState.time > 9000) {
-
-			i++;
-			if (i == 20) {
-				movementDice();
-				i = 0;
-			}
-			getNextXPosition();
-			getNextYPosition();
-			checkTileMapCollision();
-			setPosition(getXposNew(), getYposNew());
+		if(target != null) {
+			moveTowards(target);
 		}
 	}
-
-	@Override
-	public void checkYCollision() {
-		calculateCorners(xpos, ydest);
-		if (dy < 0) {
-			if (topLeftBlocked || topRightBlocked) {
-				dy = 0;
-				yposNew = currRow * tileSize + cheight / 2;
-			} else {
-				if (yposNew <= 60) {
-					yposNew = 560;
-				} else {
-					yposNew += dy;
-				}
-
-			}
-		} else if (dy > 0) {
-			if (bottomLeftBlocked || bottomRightBlocked) {
-				dy = 0;
-				falling = false;
-				yposNew = (currRow + 1) * tileSize - cheight / 2;
-			} else {
-				falling = true;
-				yposNew += dy;
-
-				if (yposNew >= tileMap.getHeight() - 15) {
-					yposNew = 3 * tileMap.getTileSize();
-				}
-			}
-
-		}
-
-		if (!falling) {
-			calculateCorners(xpos, ydest + 1);
-			if (!bottomLeftBlocked && !bottomRightBlocked) {
-				falling = true;
-			}
-		}
+	
+	public void moveTowards(MapObject mo) {
+		moveTowards(mo.getx(), mo.gety());
 	}
-
-	@Override
-	public final void draw(final Graphics2D g) {
-		 if (System.currentTimeMillis() - LevelState.time > 9000) {
-		super.draw(g);
+	
+	public void moveTowards(double x, double y) {
+		double newX = getx();
+		double newY = gety();
+		double speed = getMovSpeed();
+		if (x - speed > getx() ) {
+			newX += speed;
+			facingRight = false;
+		} else if (x + speed < getx()) {
+			newX -= speed;
+			facingRight = true;
 		}
+		if(y - speed > gety()) {
+			newY += speed;
+		} else if (y + speed < gety()) {
+			newY -= speed;
+		}
+		setPosition(newX, newY);
 	}
 
 	@Override
