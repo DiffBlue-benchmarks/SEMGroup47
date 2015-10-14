9a3ca8673e918feb582837233b26946fd3598f1f
Christian <cmaulany@tudelft.nl>
2015-10-14 12:22:12 +0200

    magiron animation
    magiron is now animated!
    <feature

--- src/main/java/sem/group47/entity/enemies/Magiron.java
+++ src/main/java/sem/group47/entity/enemies/Magiron.java
@@ -1,9 +1,19 @@
 package sem.group47.entity.enemies;
 
-import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+import org.apache.commons.io.FileUtils;
+
+import sem.group47.entity.Animation;
 import sem.group47.entity.MapObject;
-import sem.group47.entity.Player;
 import sem.group47.tilemap.TileMap;
 
 public class Magiron extends Enemy {	
@@ -21,35 +31,55 @@ public class Magiron extends Enemy {
 
 		setFacingRight(true);
 
-		if (Math.round(Math.random()) == 0) {
-			setLeft(true);
-		} else {
-			setRight(true);
-		}
-
+		BufferedImage[] animationSprites = null;
 		try {
-			setSprite(ImageIO.read(getClass().getResourceAsStream(
-					"/enemies/magiaaron.gif")));
-		} catch (Exception e) {
+			ImageReader reader = ImageIO.getImageReadersByFormatName("gif").next();
+			File input = new File("src/main/resources/enemies/magiaaron.gif");
+			ImageInputStream stream = ImageIO.createImageInputStream(input);
+			reader.setInput(stream);
+
+			int count = reader.getNumImages(true);
+			animationSprites = new BufferedImage[count];
+			for (int index = 0; index < count; index++) {
+				BufferedImage frame = reader.read(index);
+				animationSprites[index] = frame;
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		animation = new Animation();
+		animation.setFrames(animationSprites);
+		animation.setDelay(60);
 	}
-	
+
+	@Override
+	public final void draw(final Graphics2D g) {
+		if (facingRight) {
+			g.drawImage(animation.getImage(), (int) (getXpos() - getWidth() / (double) 2),
+					(int) (getYpos() - getHeight() / (double) 2), getWidth(), getHeight(), null);
+		} else {
+			g.drawImage(animation.getImage(), (int) (getXpos() + getWidth() / (double) 2),
+					(int) (getYpos() - getHeight() / (double) 2), -getWidth(), getHeight(), null);
+		}
+	}
+
 	public void setTarget(MapObject t) {
 		target = t;
 	}
 
 	@Override
 	public void update() {
+		animation.update();
 		if(target != null) {
 			moveTowards(target);
 		}
 	}
-	
+
 	public void moveTowards(MapObject mo) {
 		moveTowards(mo.getx(), mo.gety());
 	}
-	
+
 	public void moveTowards(double x, double y) {
 		double newX = getx();
 		double newY = gety();
