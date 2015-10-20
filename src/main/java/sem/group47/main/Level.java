a36d2418fdd0b8030729508f81992acc40427557
Karin van Garderen <K.A.vanGarderen@student.tudelft.nl>
2015-10-20 12:11:06 +0200

    Game is actually still working

--- src/main/java/sem/group47/main/Level.java
+++ src/main/java/sem/group47/main/Level.java
@@ -3,7 +3,7 @@ package sem.group47.main;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
-import sem.group47.entity.HUD;
+import sem.group47.audio.AudioPlayer;
 import sem.group47.entity.Player;
 import sem.group47.entity.PlayerSave;
 import sem.group47.entity.enemies.Enemy;
@@ -12,31 +12,32 @@ import sem.group47.entity.enemies.Magiron;
 import sem.group47.entity.enemies.ProjectileEnemy;
 import sem.group47.entity.pickups.BubbleSizePowerup;
 import sem.group47.entity.pickups.BubbleSpeedPowerup;
+import sem.group47.entity.pickups.Fruit;
 import sem.group47.entity.pickups.MovementSpeedPowerup;
 import sem.group47.entity.pickups.PickupObject;
 import sem.group47.tilemap.TileMap;
 
 public class Level extends DrawComposite{
 
-	private int levelNumber;
 	private ArrayList<Enemy> enemies;
 	private ArrayList<PickupObject> pickups;
 	private Magiron aaron;
 	private TileMap tileMap;
 	private Player player1;
+	private boolean multiplayer;
+	private Player player2;
+	private int levelStepCount;
+	/** Time in seconds before Aaron appears. **/
+	private static int AARON_APPEAR_DELAY = 45;
 	
-	/** level file names. **/
-	private String[] levelFileNames = new String[] {"level1.map",
-			"level2.map", "level3.map", "level4.map" };
-	
-	public Level(int num) {
-		levelNumber = num;
+	public Level(String levelFileName) {
 		enemies = new ArrayList<Enemy>();
 		pickups = new ArrayList<PickupObject>();
-		loadTileMap();
+		loadTileMap(levelFileName);
 		loadPlayers();
 		populateEnemies();
 		populatePowerups();
+		levelStepCount = 0;
 	}
 	
 	private void populateEnemies() {
@@ -67,10 +68,11 @@ public class Level extends DrawComposite{
 		addComponent(aaron);
 	};
 	
-	private void loadTileMap() {
+	private void loadTileMap(final String levelFileName) {
 		tileMap = new TileMap(30);
 		tileMap.loadTiles("/tiles/Bubble_Tile.gif");
-		tileMap.loadMap("/maps/" + levelFileNames[levelNumber]);
+		tileMap.loadMap("/maps/" + levelFileName);
+		addComponent(tileMap);
 	};
 	
 	public void loadPlayers() {
@@ -82,6 +84,18 @@ public class Level extends DrawComposite{
 		player1.setExtraLive(PlayerSave.getExtraLiveP1());
 		player1.setScore(PlayerSave.getScoreP1());
 		addComponent(player1);
+
+		if (multiplayer) {
+			player2 = new Player(tileMap);
+			player2.setPosition(
+					tileMap.getTileSize() * (tileMap.getNumCols() - 3 + .5d) - 5,
+					tileMap.getTileSize() * (tileMap.getNumRows() - 2 + .5d));
+			player2.setLives(PlayerSave.getLivesP2());
+			player2.setExtraLive(PlayerSave.getExtraLiveP2());
+			player2.setScore(PlayerSave.getScoreP2());
+			player2.setFacingRight(false);
+			addComponent(player2);
+		}
 	};
 	
 	/**
@@ -102,9 +116,169 @@ public class Level extends DrawComposite{
 		addComponent(po);
 	}
 	
+	public void update() {
+		if (player1.getLives() > 0) {
+			player1.update();
+			directEnemyCollision(player1);
+			player1.indirectEnemyCollision(enemies);
+		} else {
+			removeComponent(player1);
+		}
+		if (multiplayer) {
+			if (player2.getLives() > 0) {
+				player2.update();
+				directEnemyCollision(player2);
+				player2.indirectEnemyCollision(enemies);
+			} else {
+				removeComponent(player2);
+			}
+		}
+
+		if (levelStepCount == GamePanel.FPS * AARON_APPEAR_DELAY) {
+			targetAaron();
+		}
+		aaron.update();
+		levelStepCount++;
+
+		for (int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).update();
+			if (enemies.get(i).projectileCollision(player1)) {
+				player1.kill();
+			}
+			if (multiplayer && enemies.get(i)
+					.projectileCollision(player2)) {
+				player2.kill();
+			}
+		}
+
+		for (int i = 0; i < pickups.size(); i++) {
+			if (pickups.get(i).checkCollision(player1) || (multiplayer
+					&& pickups.get(i).checkCollision(player2))) {
+				AudioPlayer.play("extraLife");
+				removeComponent(pickups.get(i));
+				pickups.remove(i);
+
+				i--;
+			} else {
+				pickups.get(i).update();
+			}
+		}
+	}
+	
+	/**
+	 * checks what happens when the player directly collides with an enemy.
+	 *
+	 * @param player
+	 *            the Player object to check collisions with
+	 */
+	public final void directEnemyCollision(final Player player) {
+		if (player.intersects(aaron)) {
+			player.kill();
+			targetAaron();
+		}
+
+		for (int i = 0; i < enemies.size(); i++) {
+
+			if (player.intersects(enemies.get(i))) {
+				if (enemies.get(i).isCaught()) {
+					Fruit fr = new Fruit(tileMap);
+
+					if (enemies.get(i).getXpos() > 400) {
+						fr.setPosition(enemies.get(i).getXpos() - 100,
+								enemies.get(i).getYpos());
+					} else {
+						fr.setPosition(enemies.get(i).getXpos() + 100,
+								enemies.get(i).getYpos());
+					}
+					pickups.add(fr);
+					addComponent(fr);
+
+					player.setScore(enemies.get(i).getScorePoints());
+					removeComponent(enemies.get(i));
+					enemies.remove(i);
+
+					Log.info("Player Action",
+							"Player collision with Caught Enemy");
+
+				} else if (player.getLives() > 1) {
+					player.hit(1);
+					Log.info("Player Action",
+							"Player collision with Enemy");
+
+				} else {
+					AudioPlayer.play("crash");
+					player.hit(1);
+					Log.info("Player Action",
+							"Player collision with Enemy");
+				}
+			}
+		}
+
+	}
+	
+	/**
+	 * Target aaron.
+	 */
+	private void targetAaron() {
+		if (multiplayer) {
+			if (player1.getLives() > 0) {
+				if (Math.random() > .5d || player2.getLives() <= 0) {
+					aaron.setTarget(player1);
+				} else {
+					aaron.setTarget(player2);
+				}
+			} else {
+				aaron.setTarget(player2);
+			}
+		} else {
+			aaron.setTarget(player1);
+		}
+	}
+	
+	/**
+	 * checks if the player is dead.
+	 */
+	public boolean hasLost() {
+		if (player1.getLives() <= 0) {
+			removeComponent(player1);
+			if (!multiplayer || player2.getLives() <= 0) {
+
+				PlayerSave.setScoreP1(player1.getScore());
+				if (multiplayer) {
+					PlayerSave.setScoreP2(player2.getScore());
+				}
+
+				return true;
+			}
+		} else if (multiplayer && player2.getLives() <= 0) {
+			removeComponent(player2);
+		}
+		return false;
+	}
+
+	public boolean hasWon() {
+		if (enemies.size() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	@Override
 	public void draw(final Graphics2D gr) {
-		// TODO Auto-generated method stub
+		drawComponents(gr);
+	}
+
+	public Player getPlayer1() {
+		return player1;
+	}
+	
+	public Player getPlayer2() {
+		return player2;
+	}
+	
+	public TileMap getTileMap() {
+		return tileMap;
 	}
 
 
--- src/main/java/sem/group47/main/LevelFactory.java
+++ src/main/java/sem/group47/main/LevelFactory.java
@@ -2,10 +2,4 @@ package sem.group47.main;
 
 public interface LevelFactory {
 
-	void populateEnemies();
-	void loadTileMap();
-	void loadplayers();
-	void loadHUD();
-	void populatePowerups();
-	void loadAudio();
 }
